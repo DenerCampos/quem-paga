0134261abba0815d0029d553265f1a092446957c
Dener <dener.campos@monetizze.com.br>
2021-08-31 10:16:18 -0300

    feat(api): QPA-1 - Cria rotas basicas

--- src/pages/api/members.js
+++ src/pages/api/members.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { getDataBase } from '../../database/data'
+import { getMembers } from '../../database/data'
 
 export default async function handler(req, res) {
-  const data = await getDataBase()
+  const data = await getMembers()
 
   res.status(200).json(data)
 }
